d1e009f8d8bf628109d8b49a6be3baa2c69c03a5
Cipherr674 <71639314+Cipherr674@users.noreply.github.com>
2024-12-23 11:23:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 
 const basicRoutes = require('./routes/router');
 
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+
 app.use('/hospital', basicRoutes);
 
-// Start the server
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
